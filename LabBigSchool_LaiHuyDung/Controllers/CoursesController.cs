6f47b616e6d969fd0d9975f0827d61a11d5577d0
TIGER 939 <TIGER 939@DESKTOP-FSR2JLE>
2022-05-30 16:04:14 +0700

    Add project files.

---
+++ LabBigSchool_LaiHuyDung/Controllers/CoursesController.cs
@@ -0,0 +1,46 @@
+﻿using LabBigSchool_LaiHuyDung.Models;
+using LabBigSchool_LaiHuyDung.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabBigSchool_LaiHuyDung.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Courses
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ LabBigSchool_LaiHuyDung/Migrations/202205300648336_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace LabBigSchool_LaiHuyDung.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1, N'Giáo dục')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2, 'QTKD')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3, 'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
